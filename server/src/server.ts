5b3c432253793ea67faf1907c17f490bb41320cb
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-18 09:35:03 -0300

    build: install, setup prettier and eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const app = fastify();
 
-app.get('/', () => 'Hello World!')
+app.get('/', () => 'Hello World!');
 
-app.listen({
-  port: 3333,
-}).then(() => console.log("Server is running on port 3333"))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('Server is running on port 3333'));
