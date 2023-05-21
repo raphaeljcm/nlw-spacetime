71888fa0d4a7ca6b22ee98bbc2e2e613c8e4715d
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-21 17:45:13 -0300

    feat: web project - show user profile

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -1,6 +1,5 @@
+import { api } from '@/lib/api';
 import { NextRequest, NextResponse } from 'next/server';
-
-import { api } from '../../../../../lib/api';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
