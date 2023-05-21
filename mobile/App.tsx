8bc5677eadebf43062df88e73ad963da3283ae3c
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-21 12:16:15 -0300

    feat: mobile project - homepage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,67 @@
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+
+import blurBg from './assets/bg-blur.png';
+import NlwLogo from './assets/nlw-logo.svg';
+import Stripes from './assets/stripes.svg';
+
+const StylesStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) return null;
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-zinc-50">Hello World!</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+    >
+      <StylesStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          className="rounded-full bg-green-500 px-5 py-2"
+          activeOpacity={0.7}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   );
 }
