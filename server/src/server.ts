5a43f0c44d599470cc3015503e3f82a5dcf2c62d
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-21 16:54:54 -0300

    feat: server project - creating JWT token

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 import 'dotenv/config';
 import fastify from 'fastify';
 
@@ -10,6 +11,10 @@ const app = fastify();
 
 app.register(cors, {
   origin: true,
+});
+
+app.register(jwt, {
+  secret: 'spacetime',
 });
 
 app.register(authRoutes);
