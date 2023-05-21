75a95f59b46d9dd6bd3f456752e9f8cdeab054bc
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-21 16:00:53 -0300

    build: web project - removing eslint import plugin

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
-import nlwLogo from '@/assets/nlw-spacetime-logo.svg';
-import { User } from 'lucide-react';
-import Image from 'next/image';
+import { Copyright } from '@/components/Copyright';
+import { EmptyMemories } from '@/components/EmptyMemories';
+import { Hero } from '@/components/Hero';
+import { SignIn } from '@/components/SignIn';
 
 export default function Home() {
   return (
@@ -9,67 +10,15 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
+        <SignIn />
 
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
+        <Hero />
 
-        <article className="space-y-5">
-          <Image src={nlwLogo} alt="Nlw Spacetime logo" />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            href="#create-memory"
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
-          >
-            Cadastrar lembrança
-          </a>
-        </article>
-
-        <article className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a
-            href="https://rocketseat.com.br"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline transition-colors hover:text-gray-100"
-          >
-            Rocketseat
-          </a>
-        </article>
+        <Copyright />
       </section>
 
       <section className="flex flex-col bg-[url(/bg-stars.svg)] bg-cover p-16">
-        <article className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a
-              href="#create-memory"
-              className="underline transition-colors hover:text-gray-50"
-            >
-              criar agora
-            </a>
-            !
-          </p>
-        </article>
+        <EmptyMemories />
       </section>
     </main>
   );
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <article className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        href="https://rocketseat.com.br"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline transition-colors hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+    </article>
+  );
+}
