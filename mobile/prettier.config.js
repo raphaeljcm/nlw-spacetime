3e759fe576de3ba044ff5c4da39828246974693b
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-21 18:04:01 -0300

    build: mobile project - removing prettier import plugin

--- mobile/prettier.config.js
+++ mobile/prettier.config.js
@@ -2,8 +2,5 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
-  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
   plugins: [require('prettier-plugin-tailwindcss')],
 };
