69bbd908b0cf76fe4fc9691e53d1355818d1289e
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-22 21:38:48 -0300

    refactor: using layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,6 +4,11 @@ import {
 } from 'next/font/google';
 
 import './globals.css';
+import { Profile } from '@/components/Profile';
+import { SignIn } from '@/components/SignIn';
+import { Hero } from '@/components/Hero';
+import { Copyright } from '@/components/Copyright';
+import { cookies } from 'next/headers';
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' });
 
@@ -24,12 +29,29 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isAuthenticated = cookies().has('token');
+
   return (
     <html lang="pt-BR">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          <section className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(/bg-stars.svg)] bg-cover px-28 py-16">
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+
+            <Hero />
+
+            <Copyright />
+          </section>
+
+          <section className="flex flex-col bg-[url(/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </section>
+        </main>
       </body>
     </html>
   );
