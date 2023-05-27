ce6e91de926eebe85789be4e21cd422b7162717b
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-27 17:16:59 -0300

    feat: server project - file upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,23 @@ import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
 import 'dotenv/config';
 import fastify from 'fastify';
+import multipart from '@fastify/multipart';
 
 import { authRoutes } from './routes/auth';
 import { memoriesRoutes } from './routes/memories';
 import { usersRoutes } from './routes/users';
+import { uploadRoutes } from './routes/upload';
+import staticPlugin from '@fastify/static';
+import { resolve } from 'path';
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(staticPlugin, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+});
 
 app.register(cors, {
   origin: true,
@@ -17,6 +28,7 @@ app.register(jwt, {
   secret: 'spacetime',
 });
 
+app.register(uploadRoutes);
 app.register(authRoutes);
 app.register(memoriesRoutes);
 app.register(usersRoutes);
