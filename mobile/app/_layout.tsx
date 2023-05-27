3693b3fb34331f783a2900e89598d02628bbc0b7
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-27 16:38:30 -0300

    feat: mobile project - new memory page

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -53,6 +53,7 @@ export default function Layout() {
         }}
       >
         <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
         <Stack.Screen name="memories" />
       </Stack>
     </ImageBackground>
